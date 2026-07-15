e4f8cdd4db5b7d942ccff792cab67f5ce0a4f515
kimseongyeon <seongyeon0810@naver.com>
2026-07-16 01:07:29 +0900

    fix: DetailDropDown 디자인 수정

--- src/features/city-insight/components/DetailDropDown.tsx
+++ src/features/city-insight/components/DetailDropDown.tsx
@@ -21,19 +21,19 @@ export default function DetailDropDown() {
     }
 
   return (
-    <div className="relative">
+    <div className="relative cursor-pointer">
         <div className={`inline-flex justify-center items-center gap-1 rounded-2 py-1.5 px-2 cursor-pointer ${isOpen ? 'bg-primary-500' : 'bg-gray-50 hover:bg-gray-100'}`} onClick={handleOpen}>
             <FilterIcon color={isOpen ? '#ffffff' : undefined}/>
             <p className={`body-03 ${isOpen ? 'text-white' : 'text-gray-600'}`}>상세필터</p>
         </div>
         {isOpen && (
         <div className='absolute top-10 z-1 bg-white w-[624px] h-[242px] flex flex-col justify-center items-center border border-gray-100 rounded-2 px-6 py-5 shadow-01'>
-            {DETAIL_OPTIONS.map((item) => (
-                <div key={item.title} className="w-full flex items-center mb-3">
+            {DETAIL_OPTIONS.map((item, index) => (
+                <div key={item.title} className={`w-full flex items-center ${index !== DETAIL_OPTIONS.length - 1 ? 'mb-3' : ''}`}>
                     <div className="flex items-center w-25 shrink-0">
                         <p className="flex-1 text-gray-700 body-02">{item.title}</p>
                         <div className="w-px h-4 bg-gray-200" />
-                    </div>
+                    </div> 
                     <div className="flex ml-5">
                         {item.options.map((option) => {
                             const isSelected = selectedOptions[item.title] === option
