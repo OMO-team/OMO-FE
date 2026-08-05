00548c428824c81abb7fabeedae85c78e7c3becb
kimseongyeon <seongyeon0810@naver.com>
2026-08-06 01:40:12 +0900

    feat: 스마트 브리핑 플로팅 버튼 구현

---
+++ src/shared/components/SmartBriefingFAB.tsx
@@ -0,0 +1,14 @@
+import { useNavigate } from "react-router-dom"
+import SmartBriefingIcon from "./SmartBriefingIcon"
+
+export default function SmartBriefingFAB() {
+    const navigate = useNavigate();
+  return (
+    <button onClick={() => navigate('/')} className="group h-16 overflow-hidden max-w-16 hover:max-w-[180px] px-5 bg-black rounded-[12px] flex justify-center items-center fixed bottom-10 right-[30px] z-50 transition-[max-width] duration-300 whitespace-nowrap">
+      <SmartBriefingIcon type="white" />
+      <span className="title-02 text-white overflow-hidden max-w-0 opacity-0 group-hover:max-w-[120px] group-hover:opacity-100 group-hover:pl-2.5 transition-all duration-300">
+        스마트 브리핑
+      </span>
+    </button>
+  )
+}
