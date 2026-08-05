203d7cf57a33c92ece9721dd20ff4ab08ed5ba32
kimseongyeon <seongyeon0810@naver.com>
2026-08-06 01:53:28 +0900

    fix: 플로팅 버튼 클릭 시 사이드바 오픈

--- src/shared/components/SmartBriefingFAB.tsx
+++ src/shared/components/SmartBriefingFAB.tsx
@@ -1,10 +1,10 @@
-import { useNavigate } from "react-router-dom"
+import { useMainLayoutContext } from "../layouts/useMainLayoutContext"
 import SmartBriefingIcon from "./SmartBriefingIcon"
 
 export default function SmartBriefingFAB() {
-    const navigate = useNavigate();
+  const { openChat } = useMainLayoutContext();
   return (
-    <button onClick={() => navigate('/')} className="group h-16 overflow-hidden max-w-16 hover:max-w-[180px] px-5 bg-black rounded-[12px] flex justify-center items-center fixed bottom-10 right-[30px] z-50 transition-[max-width] duration-300 whitespace-nowrap">
+    <button onClick={() => openChat()} className="group h-16 overflow-hidden max-w-16 hover:max-w-[180px] px-5 bg-black rounded-[12px] flex justify-center items-center fixed bottom-10 right-[30px] z-50 transition-[max-width] duration-300 whitespace-nowrap">
       <SmartBriefingIcon type="white" />
       <span className="title-02 text-white overflow-hidden max-w-0 opacity-0 group-hover:max-w-[120px] group-hover:opacity-100 group-hover:pl-2.5 transition-all duration-300">
         스마트 브리핑
